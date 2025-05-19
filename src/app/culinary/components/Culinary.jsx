64f8cea1519c2df2b54ed4d92252a9d761a86d76
Youssef97dev <youssefmoussafi97@gmail.com>
2025-05-19 15:05:44 +0100

    change typo 3

--- src/app/culinary/components/Culinary.jsx
+++ src/app/culinary/components/Culinary.jsx
@@ -6,7 +6,7 @@ const Culinary = () => {
   const { t } = useTranslation();
   return (
     <div className="w-full flex flex-col justify-start items-start gap-5 px-5 lg:px-10 text-primary">
-      <b className="text-[23px] tracking-wider lg:text-[40px] lg:leading-[48px] leading-[36px] font-azahra">
+      <b className="text-[20px] tracking-wider lg:text-[40px] lg:leading-[48px] leading-[36px] font-azahra">
         {t("culinary.title")}
       </b>
       <p className="text-[12px] lg:text-[15px] leading-[24.5px]">
