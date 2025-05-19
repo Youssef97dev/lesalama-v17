c87e1695b33e0ea4c92917e6dbdfc382717a8880
Youssef97dev <youssefmoussafi97@gmail.com>
2025-05-19 15:00:02 +0100

    change pictures 2

--- src/app/culinary/components/Culinary.jsx
+++ src/app/culinary/components/Culinary.jsx
@@ -6,7 +6,7 @@ const Culinary = () => {
   const { t } = useTranslation();
   return (
     <div className="w-full flex flex-col justify-start items-start gap-5 px-5 lg:px-10 text-primary">
-      <h1 className="text-[23px] lg:text-[40px] lg:leading-[48px] leading-[36px] font-azahra">
+      <h1 className="text-[28px] lg:text-[40px] lg:leading-[48px] leading-[36px] font-azahra">
         {t("culinary.title")}
       </h1>
       <p className="text-[12px] lg:text-[15px] leading-[24.5px]">
